a805c6700bca77a7f20393a0820f505d452cdbbd
thomas bozeman <tboz203@gmail.com>
2024-04-02 22:10:31 -0500

    More Fancy + Black Formatting

--- django_spellbook/admin.py
+++ django_spellbook/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from django_spellbook.models import Spell
 
+
 @admin.register(Spell)
 class SpellAdmin(admin.ModelAdmin):
-    list_display_links = ['name']
+    list_display_links = ["name"]
     list_display = [
-        'level', 'name', 'school', 'range', 'components', 'duration', 'concentration', 'ritual'
+        "pretty_level",
+        "name",
+        "pretty_school",
+        "range",
+        "short_components",
+        "duration",
+        "concentration",
+        "ritual",
     ]
-    search_fields = ['name', 'school', 'description', 'description_high']
-    list_filter = ['level', 'concentration', 'ritual', 'favorite']
+    search_fields = ["name", "school", "description", "description_high"]
+    list_filter = ["level", "concentration", "ritual", "favorite"]
--- django_spellbook/models.py
+++ django_spellbook/models.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.db import models
 
+
+def ordinal(n):
+    suffixes = {1: "st", 2: "nd", 3: "rd"}
+    return str(n) + ("th" if 10 < abs(n) < 14 else suffixes.get(abs(n) % 10, "th"))
+
+
 class Spell(models.Model):
     """Spells"""
+
     _id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=1000, unique=True)
     school = models.CharField(max_length=1000)
@@ -22,10 +29,25 @@ class Spell(models.Model):
     concentration = models.BooleanField()
     sound = models.CharField(max_length=100, blank=True)
 
+    @property
+    @admin.display(description="Level", ordering="level")
+    def pretty_level(self):
+        return "Cantrip" if self.level == -1 else ordinal(self.level)
+
+    @property
+    @admin.display(description="School", ordering="school")
+    def pretty_school(self):
+        return self.school.split()[0]
+
+    @property
+    @admin.display(description="Components", ordering="components")
+    def short_components(self):
+        return self.components.split("(")[0].strip()
+
     def __str__(self):
         return self.name
 
     class Meta:
         managed = False
-        db_table = 'spell'
-        ordering = ['level', 'name']
+        db_table = "spell"
+        ordering = ["level", "name"]
